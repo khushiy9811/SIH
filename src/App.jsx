85717e65ad86bda6e68c9a980c4952980e9d3944
khushiy9811 <151054071+khushiy9811@users.noreply.github.com>
2025-09-07 14:37:46 +0530

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import ScreeningModule from "./PHQ9Module";
-
-export default function App() {
-  return <ScreeningModule onNavigate={(to) => {
-    if (to === "resources") window.location.href = "/resources";
-    if (to === "booking") window.location.href = "/booking";
-  }} />;
-}
